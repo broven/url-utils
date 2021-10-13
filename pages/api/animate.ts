50fb3afc842b9c75a91a29ec941fdfe54e1765d9
metajs <zycode277@gmail.com>
2021-10-13 18:57:24 +0800

    update: stash

--- pages/api/animate.ts
+++ pages/api/animate.ts
@@ -6,7 +6,42 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 import convert from 'xml-js';
 
-const isTraditionalChinese = (text: string) => {
+export enum IResolution {
+  rUHD = 'UHD',
+  r1080p = '1080p',
+  r720p = '720p',
+  rUnknown = 'unknown'
+}
+export class AnimateTitleMetaParser {
+  private rawTitle: string;
+  constructor(title: string) {
+    this.rawTitle = title;
+  }
+  get resolution(): IResolution {
+    const t = this.rawTitle;
+    switch(true) {
+      case t.indexOf('1080p') !== -1:
+      case t.indexOf('1080P') !== -1:
+      case t.indexOf('1920X1080') !== -1:
+        return IResolution.r1080p;
+      case t.indexOf('720p') !== -1:
+      case t.indexOf('1280X720') !== -1:
+        return IResolution.r720p;
+      default:
+        return IResolution.rUnknown;
+    }
+  }
+  get isSeasonPack(): boolean {
+    const title = this.rawTitle
+    switch(true) {
+        case title.indexOf('【合集】') !== -1:
+            return true;
+        default:
+            return false;
+    }
+  }
+  get isTraditionalChinese(): boolean {
+    const text = this.rawTitle;
     switch(true) {
         case text.indexOf('BIG5') !== -1:
         case text.indexOf('CHT') !== -1:
@@ -14,29 +49,43 @@ const isTraditionalChinese = (text: string) => {
         default:
             return false;
     }
-}
-const isSeasonPack = (title: string) => {
-    switch(true) {
-        case title.indexOf('【合集】') !== -1:
-            return true;
-        default:
-            return false;
+  }
+  get season(): number {
+    return -1;
+  }
+  get episode(): number {
+    const splitterArr = [
+      /】/, /【/, /\[/, /\]/, /\s/
+    ];
+    const regArr = [
+      /~~~(\d{1,})~~~/
+    ];
+    let title = this.rawTitle;
+    for (const splitter of splitterArr) {
+      const _reg = new RegExp(splitter, 'g');
+      title = title.replace(_reg, '~~~');
     }
+    console.log(title);
+    for (const reg of regArr) {
+      const match = reg.exec(this.rawTitle);
+      if (match) {
+        return parseInt(match[1]);
+      }
+    }
+    return -1;
+  }
 }
 export const animateFilter = (content: string) => {
   const parsedFeed:any = convert.xml2js(content, { compact: true});
   parsedFeed.rss.channel.item = parsedFeed.rss.channel.item.map((item: any) => {
     const title = item.title._text;
-    if (isSeasonPack(title)) return null;
-    if (isTraditionalChinese(title)) return null;
+    const metaParser = new AnimateTitleMetaParser(title);
+    if (metaParser.isSeasonPack) return null;
+    if (metaParser.isTraditionalChinese) return null;
    return item;
   }).filter((i: any) => i !== null);
   return convert.js2xml(parsedFeed, {compact: true, spaces: 4});
 }
-
-
-
-
 
 export default async function AnimateTransformer(
   req: NextApiRequest,
--- test/animte/animate.test.ts
+++ test/animte/animate.test.ts
@@ -1,4 +1,4 @@
-import { animateFilter } from '@api/animate'
+import { animateFilter, AnimateTitleMetaParser, IResolution } from '@api/animate'
 import fs from 'fs';
 // https://mikanani.me/
 // TODO: axios jest找不到， 看看解决方案
@@ -23,5 +23,28 @@ describe('幻樱字幕组', () => {
     //     const result = animateFilter(content.toString());
     //     expect(result).toMatchSnapshot();
     // });
+
+})
+describe.each([
+    ['【动漫国字幕组】★07月新番[碧水白沙 / 白沙的水族馆][11-14][1080P][繁体][MP4]', IResolution.r1080p, false, false, -1, -1], // 这种的集数简直毒瘤
+    ['[喵萌奶茶屋&LoliHouse] 平家物语 / Heike Monogatari - 03 [WebRip 1080p HEVC-10bit AAC][简繁日内封字幕]', IResolution.r1080p, false, false, -1, 3],
+    ['【喵萌Production】★07月新番★[Love Live! Superstar!! / ラブライブ！スーパースター!!][11][1080p][繁日双语][招募翻译]', IResolution.r1080p, false, false, -1, 11],
+])('AnimateTitleMetaParser %s', (title, resolution, isSeasonPack, isChinese, season, ep) => {
+    const metaParser = new AnimateTitleMetaParser(title);
+    test('分辨率', () => {
+        expect(metaParser.resolution).toBe(resolution);
+    })
+    test('是否是合集', () => {
+        expect(metaParser.isSeasonPack).toBe(isSeasonPack);
+    })
+    test('中文识别', () => {
+          expect(metaParser.isTraditionalChinese).toEqual(isChinese)
+    })
+    test('season', () => {
+        expect(metaParser.season).toBe(season);
+    });
+    test('episode', () => {
+        expect(metaParser.episode).toBe(ep);
+    })
 
 })
